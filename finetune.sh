057348ab2fe249ea90c0670f9cb384261be90640
ahmed <aadel@umd.edu>
2024-03-03 23:08:43 -0500

    bug fix in manifest. nest quotation marks messed up spaces and escaped spaces

--- finetune.sh
+++ finetune.sh
@@ -43,9 +43,8 @@ save_dir=$fold
 
 manifest_path=$curr_dir/manifest/$dataset
 #replace spaces in $fold with escaped space
-fold=${fold// /\\ }
-args="task.data=$curr_dir/manifest/$fold "
-args+="model.w2v_path=$model_path "
+args=task.data="$manifest_path/$fold "
+args+=" model.w2v_path"="$model_path "
 if [[ $resume == "true" ]] ; then
   restore_file=$(ls -d $outdir_fold/$fold/*/*/* | tail -n 1)/checkpoint_last.pt
   #assert that the file exists
@@ -54,15 +53,15 @@ if [[ $resume == "true" ]] ; then
     exit 1
   fi
 
-  args+="checkpoint.restore_file=$restore_file "
+  args+=checkpoint.restore_file=$restore_file 
 fi
 
-args+="common.wandb_project=$wandb_project "
-args+="checkpoint.save_dir=./$fold "
-args+="distributed_training.distributed_world_size=1 "
-args+="--config-dir $curr_dir/config/ "
+args+=" common.wandb_project"="$wandb_project "
+args+=" checkpoint.save_dir"="./$fold "
+args+=" distributed_training.distributed_world_size=1 "
+args+="--config-dir $curr_dir/config/"
 args+="--config-name $config_name"
 
 
 echo args: $args
-fairseq-hydra-train $args
+fairseq-hydra-train "$args"
