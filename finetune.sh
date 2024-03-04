a58d5d6e98a4c3acf1f657328a509dfc4b3af669
ahmed <aadel@umd.edu>
2024-03-03 22:51:06 -0500

    bug fix

--- finetune.sh
+++ finetune.sh
@@ -44,7 +44,7 @@ save_dir=$fold
 manifest_path=$curr_dir/manifest/$dataset
 
 args="task.data="$curr_dir/manifest/$fold""
-args+="model.w2v_path='$model_path' "
+args+="model.w2v_path="$model_path""
 if [[ $resume == "true" ]] ; then
   restore_file=$(ls -d $outdir_fold/$fold/*/*/* | tail -n 1)/checkpoint_last.pt
   #assert that the file exists
@@ -53,11 +53,11 @@ if [[ $resume == "true" ]] ; then
     exit 1
   fi
 
-  args+="checkpoint.restore_file=""$restore_file" "
+  args+="checkpoint.restore_file="$restore_file""
 fi
 
 args+="common.wandb_project="$wandb_project""
-args+="checkpoint.save_dir="./$fold" "
+args+="checkpoint.save_dir="./$fold""
 args+="distributed_training.distributed_world_size=1 "
 args+="--config-dir $curr_dir/config/ "
 args+="--config-name $config_name"
