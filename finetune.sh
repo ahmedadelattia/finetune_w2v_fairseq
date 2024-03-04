34fe4a780aa1beafb5d5cf57862828d3abb8a752
ahmed <aadel@umd.edu>
2024-03-03 22:55:49 -0500

    replace spaces in $fold with escaped space

--- finetune.sh
+++ finetune.sh
@@ -42,9 +42,10 @@ echo "Running on fold: $fold"
 save_dir=$fold
 
 manifest_path=$curr_dir/manifest/$dataset
-
-args="task.data=\"$curr_dir/manifest/$fold\" "
-args+="model.w2v_path=\"$model_path\" "
+#replace spaces in $fold with escaped space
+fold=${fold// /\\ }
+args="task.data=$curr_dir/manifest/$fold "
+args+="model.w2v_path=$model_path "
 if [[ $resume == "true" ]] ; then
   restore_file=$(ls -d $outdir_fold/$fold/*/*/* | tail -n 1)/checkpoint_last.pt
   #assert that the file exists
@@ -53,11 +54,11 @@ if [[ $resume == "true" ]] ; then
     exit 1
   fi
 
-  args+="checkpoint.restore_file=\"$restore_file\" "
+  args+="checkpoint.restore_file=$restore_file "
 fi
 
-args+="common.wandb_project=\"$wandb_project\" "
-args+="checkpoint.save_dir=\"./$fold\" "
+args+="common.wandb_project=$wandb_project "
+args+="checkpoint.save_dir=./$fold "
 args+="distributed_training.distributed_world_size=1 "
 args+="--config-dir $curr_dir/config/ "
 args+="--config-name $config_name"
